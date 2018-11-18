b37400bb03f67a458dbf9528265866e504ef4380
Pedro de León <dleonmxn@hotmail.com>
2018-11-18 16:44:27 -0600

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -74,12 +74,12 @@ $active_group = 'default';
 $query_builder = TRUE;
 
 $db['default'] = array(
-	'dsn'	=> '',
+	//'dsn'	=> '',
 	'hostname' => 'ec2-50-19-249-121.compute-1.amazonaws.com',
 	'username' => 'rtbzonpplyyrpl',
 	'password' => '52d15625e39e4b5b7526e803860d4c213665546b3b9c9daabd2fa0bc5a1cb520',
 	'database' => 'd6hruvc6kfho1c',
-	'dbdriver' => '',
+	'dbdriver' => 'postgre',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
