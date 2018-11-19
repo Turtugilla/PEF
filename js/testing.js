2fafa77c1ccdd3cc7d35486ff70bacf87ee09fbf
Pedro de León <dleonmxn@hotmail.com>
2018-11-19 17:29:53 -0600

    testing para conectar con banco

---
+++ js/testing.js
@@ -0,0 +1,17 @@
+function postTOBank(){
+	var miCuentaBancaria = {
+		 ctaorigen : "1445522",
+		 ctadestino : "1442233",
+		 monto : 200,
+		 detalle : "detalle"
+	};
+
+    $.ajax({
+		type: 'POST',
+		url: "https://spbank.herokuapp.com/api/payment",
+		data: miCuentaBancaria,
+		success: function(data){
+			console.log(data);
+		}
+	});
+}
--- testing.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: DLeon
- * Date: 17/11/2018
- * Time: 15:32
- */
