c8d50ac2a0078a95e9ef6b12f7dcfc2617fcc501
Pedro de León <dleonmxn@hotmail.com>
2018-11-19 10:36:27 -0600

    Update insert.php
    Testeando registro de usuarios desde webpage

--- insert.php
+++ insert.php
@@ -9,10 +9,13 @@ $city = $_POST["city"];
 $pin = $_POST["pin"];
 $email = $_POST["email"];
 $pwd = $_POST["pwd"];
+$query = "INSERT INTO users (fname, lname, address, city, pin, email , password)
+           VALUES('$fname', '$lname', '$address', '$city', $pin, '$email', '$pwd')";
 
-if($mysqli->query("INSERT INTO users (fname, lname, address, city, pin, email, password) VALUES('$fname', '$lname', '$address', '$city', $pin, '$email', '$pwd')")){
-	echo 'Data inserted';
-	echo '<br/>';
+
+if(pg_query($db_connection, $query)){
+echo 'Data inserted';
+echo '<br/>';
 }
 
 header ("location:login.php");
