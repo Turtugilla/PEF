544cb296962d7369fc50b03e169c1e894341dd19
Pedro de León <dleonmxn@hotmail.com>
2018-11-18 20:08:17 -0600

    Update Examples.php

--- application/controllers/Examples.php
+++ application/controllers/Examples.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class examples extends CI_Controller {
+class Examples extends CI_Controller {
 
 	public function __construct()
 	{
