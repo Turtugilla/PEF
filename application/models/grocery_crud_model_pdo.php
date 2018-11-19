83abfcb4fe1ae84e4a080448848b9cacdd94353e
Pedro de León <dleonmxn@hotmail.com>
2018-11-18 21:50:10 -0600

    testing

--- application/models/grocery_crud_model_pdo.php
+++ application/models/grocery_crud_model_pdo.php
@@ -7,7 +7,7 @@ class grocery_crud_model_PDO extends grocery_CRUD_Generic_Model{
         parent::__construct();
         $this->subdriver = $this->db->subdriver;
         // these drivers doesn't have CONCAT command
-        if(in_array($this->subdriver, array('sqlite', 'postgre'))){
+        if(in_array($this->subdriver, array('sqlite', 'pgsql'))){
             $this->CAPABLE_CONCAT = FALSE;
         }
     }
@@ -25,7 +25,7 @@ class grocery_crud_model_PDO extends grocery_CRUD_Generic_Model{
             $table_name = $this->table_name;
         }
         // postgre need this
-        if($this->subdriver=='postgre'){
+        if($this->subdriver=='pgsql'){
             $SQL = "SELECT
                   pg_attribute.attname
                 FROM pg_index, pg_class, pg_attribute
@@ -45,14 +45,14 @@ class grocery_crud_model_PDO extends grocery_CRUD_Generic_Model{
     function get_field_types_basic_table()
     {
         $results = parent::get_field_types_basic_table();
-        if($this->subdriver == 'postgre'){
+        if($this->subdriver == 'pgsql'){
             $results = array_reverse($results);
         }
         return $results;
     }
 
     function build_db_join_relation($related_table, $unique_name, $related_primary_key, $field_name){
-        if($this->subdriver=='postgre'){
+        if($this->subdriver=='pgsql'){
             $this->db->join( $this->protect_identifiers($related_table).' as '.$this->protect_identifiers($unique_name) ,
                 'cast('.$this->protect_identifiers($unique_name.'.'.$related_primary_key).' as character varying) = cast('. $this->protect_identifiers($this->table_name.'.'.$field_name).' as character varying)', 'left', FALSE);
 
@@ -62,7 +62,7 @@ class grocery_crud_model_PDO extends grocery_CRUD_Generic_Model{
     }
 
     function build_relation_n_n_subquery($field, $selection_table, $relation_table, $primary_key_alias_to_selection_table, $primary_key_selection_table, $primary_key_alias_to_this_table, $field_name){
-        if($this->subdriver=='postgre'){
+        if($this->subdriver=='pgsql'){
             return "(SELECT string_agg(".$this->protect_identifiers($field).", ',') FROM ".$this->protect_identifiers($selection_table)
                 ." LEFT JOIN ".$this->protect_identifiers($relation_table)." ON ".$this->protect_identifiers($relation_table.".".$primary_key_alias_to_selection_table)." = ".$this->protect_identifiers($selection_table.".".$primary_key_selection_table)
                 ." WHERE cast(".$this->protect_identifiers($relation_table.".".$primary_key_alias_to_this_table)." as character varying ) = cast(".$this->protect_identifiers($this->table_name.".".$this->get_primary_key())."  as character varying ) GROUP BY ".$this->protect_identifiers($relation_table.".".$primary_key_alias_to_this_table).") AS ".$this->protect_identifiers($field_name);
