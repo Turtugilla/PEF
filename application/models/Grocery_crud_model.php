f82f48f10c01368b574a021bd4d6bade688bf4c8
Pedro de León <dleonmxn@hotmail.com>
2018-11-18 23:34:38 -0600

    update

--- application/models/Grocery_crud_model.php
+++ application/models/Grocery_crud_model.php
@@ -1,64 +1,53 @@
 <?php
-class Grocery_crud_model {
+/**
+ * PHP grocery CRUD
+ *
+ * LICENSE
+ *
+ * Grocery CRUD is released with dual licensing, using the GPL v3 (license-gpl3.txt) and the MIT license (license-mit.txt).
+ * You don't have to do anything special to choose one license or the other and you don't have to notify anyone which license you are using.
+ * Please see the corresponding license file for details of these licenses.
+ * You are free to use, modify and distribute this software, but all copyright information must remain.
+ *
+ * @package    	grocery CRUD
+ * @copyright  	Copyright (c) 2010 through 2012, John Skoumbourdis
+ * @license    	https://github.com/scoumbourdis/grocery-crud/blob/master/license-grocery-crud.txt
+ * @version    	1.4.2
+ * @author     	John Skoumbourdis <scoumbourdisj@gmail.com>
+ */
 
-	public $ESCAPE_CHAR = '"';
-	public $CAPABLE_CONCAT = TRUE;
+// ------------------------------------------------------------------------
 
-	/*public function __construct(){
+/**
+ * Grocery CRUD Model
+ *
+ *
+ * @package    	grocery CRUD
+ * @author     	John Skoumbourdis <scoumbourdisj@gmail.com>
+ * @version    	1.5.6
+ * @link		http://www.grocerycrud.com/documentation
+ */
+class Grocery_crud_model  extends CI_Model  {
+
+	protected $primary_key = null;
+	protected $table_name = null;
+	protected $relation = array();
+	protected $relation_n_n = array();
+	protected $primary_keys = array();
+
+	function __construct()
+	{
 		parent::__construct();
-		// this is a simple hack to get ESCAPE_CHAR
-		$test = $this->protect_identifiers('t');
-		$first_char = substr($test,0,1);
-		if($first_char !== 't'){
-			$this->ESCAPE_CHAR = $first_char;
-		}
-	}*/
+	}
+
+	function db_table_exists($table_name = null)
+	{
+		return $this->db->table_exists($table_name);
+	}
 
 	public function protect_identifiers($value)
 	{
 		return $this->db->protect_identifiers($value);
-	}
-
-	// rather than mess around with this everytime, it is better to build a function for this.
-	public function build_concat_from_template($template, $prefix_replacement='', $suffix_replacement='', $as=NULL){
-		if($this->CAPABLE_CONCAT){
-			// if CONCAT is possible in the current driver
-			$concat_str =
-				"CONCAT('".
-				str_replace(
-					array(
-						"{",
-						"}"
-					),
-					array(
-						"',COALESCE(".$prefix_replacement,
-						$suffix_replacement.", ''),'"
-					),
-					str_replace("'","\\'",$template)
-				).
-				"')";
-
-		}else{
-			// if CONCAT is impossible in the current driver, use || instead
-			$concat_str =
-				"('".
-				str_replace(
-					array(
-						"{",
-						"}"
-					),
-					array(
-						"' || COALESCE(".$replacement,
-						", '') || '"
-					),
-					str_replace("'","\\'",$template)
-				).
-				"')";
-		}
-		if(isset($as)){
-			$concat_str .= " as ".$as;
-		}
-
 	}
 
 	function get_list()
@@ -124,6 +113,20 @@ class Grocery_crud_model {
 		return $results;
 	}
 
+	public function get_row($table_name = null)
+	{
+		$table_name = $table_name === null ? $this->table_name : $table_name;
+
+		return $this->db->get($table_name)->row();
+	}
+
+	public function set_primary_key($field_name, $table_name = null)
+	{
+		$table_name = $table_name === null ? $this->table_name : $table_name;
+
+		$this->primary_keys[$table_name] = $field_name;
+	}
+
 	protected function relation_n_n_queries($select)
 	{
 		$this_table_primary_key = $this->get_primary_key();
@@ -140,21 +143,64 @@ class Grocery_crud_model {
 			if($use_template)
 			{
 				$title_field_selection_table = str_replace(" ", "&nbsp;", $title_field_selection_table);
-				$field .= $this->build_concat_from_template($this->protect_identifiers($title_field_selection_table));
-				//$field .= "CONCAT('".str_replace(array('{','}'),array("',COALESCE(",", ''),'"),str_replace("'","\\'",$this->protect_identifiers($title_field_selection_table)))."')";
+				$field .= "CONCAT('".str_replace(array('{','}'),array("',COALESCE(",", ''),'"),str_replace("'","\\'",$title_field_selection_table))."')";
 			}
 			else
 			{
-				$field .= $this->protect_identifiers($selection_table.'.'.$title_field_selection_table);
+				$field .= "$selection_table.$title_field_selection_table";
 			}
 
 			//Sorry Codeigniter but you cannot help me with the subquery!
-			$select .= ", ".
-				$this->build_relation_n_n_subquery($field, $selection_table, $relation_table, $primary_key_alias_to_selection_table, $primary_key_selection_table, $primary_key_alias_to_this_table, $field_name);
+			$select .= ", (SELECT GROUP_CONCAT(DISTINCT $field) FROM $selection_table "
+				."LEFT JOIN $relation_table ON $relation_table.$primary_key_alias_to_selection_table = $selection_table.$primary_key_selection_table "
+				."WHERE $relation_table.$primary_key_alias_to_this_table = `{$this->table_name}`.$this_table_primary_key GROUP BY $relation_table.$primary_key_alias_to_this_table) AS $field_name";
 		}
 
 		return $select;
 	}
+
+	function order_by($order_by , $direction)
+	{
+		$this->db->order_by( $order_by , $direction );
+	}
+
+	function where($key, $value = NULL, $escape = TRUE)
+	{
+		$this->db->where( $key, $value, $escape);
+	}
+
+	function or_where($key, $value = NULL, $escape = TRUE)
+	{
+		$this->db->or_where( $key, $value, $escape);
+	}
+
+	function having($key, $value = NULL, $escape = TRUE)
+	{
+		$this->db->having( $key, $value, $escape);
+	}
+
+	function or_having($key, $value = NULL, $escape = TRUE)
+	{
+		$this->db->or_having( $key, $value, $escape);
+	}
+
+	function like($field, $match = '', $side = 'both')
+	{
+		$this->db->like($field, $match, $side);
+	}
+
+	function or_like($field, $match = '', $side = 'both')
+	{
+		$this->db->or_like($field, $match, $side);
+	}
+
+	function limit($value, $offset = '')
+	{
+		$this->db->limit( $value , $offset );
+	}
+
+
+
 
 	function get_total_results()
 	{
@@ -172,6 +218,24 @@ class Grocery_crud_model {
 		return $this->db->count_all_results($this->table_name);
 	}
 
+	function set_basic_table($table_name = null)
+	{
+		if( !($this->db->table_exists($table_name)) )
+			return false;
+
+		$this->table_name = $table_name;
+
+		return true;
+	}
+
+	function get_edit_values($primary_key_value)
+	{
+		$primary_key_field = $this->get_primary_key();
+		$this->db->where($primary_key_field,$primary_key_value);
+		$result = $this->db->get($this->table_name)->row();
+		return $result;
+	}
+
 	function join_relation($field_name , $related_table , $related_field_title)
 	{
 		$related_primary_key = $this->get_primary_key($related_table);
@@ -179,7 +243,7 @@ class Grocery_crud_model {
 		if($related_primary_key !== false)
 		{
 			$unique_name = $this->_unique_join_name($field_name);
-			$this->build_db_join_relation($related_table, $unique_name, $related_primary_key, $field_name);
+			$this->db->join( $related_table.' as '.$unique_name , "$unique_name.$related_primary_key = {$this->table_name}.$field_name",'left');
 
 			$this->relation[$field_name] = array($field_name , $related_table , $related_field_title);
 
@@ -189,6 +253,21 @@ class Grocery_crud_model {
 		return false;
 	}
 
+	function set_relation_n_n_field($field_info)
+	{
+		$this->relation_n_n[$field_info->field_name] = $field_info;
+	}
+
+	protected function _unique_join_name($field_name)
+	{
+		return 'j'.substr(md5($field_name),0,8); //This j is because is better for a string to begin with a letter and not with a number
+	}
+
+	protected function _unique_field_name($field_name)
+	{
+		return 's'.substr(md5($field_name),0,8); //This s is because is better for a string to begin with a letter and not with a number
+	}
+
 	function get_relation_array($field_name , $related_table , $related_field_title, $where_clause, $order_by, $limit = null, $search_like = null)
 	{
 		$relation_array = array();
@@ -196,22 +275,16 @@ class Grocery_crud_model {
 
 		$related_primary_key = $this->get_primary_key($related_table);
 
-		$select = $this->protect_identifiers($related_table).'.'.$this->protect_identifiers($related_primary_key).', ';
+		$select = "$related_table.$related_primary_key, ";
 
 		if(strstr($related_field_title,'{'))
 		{
 			$related_field_title = str_replace(" ", "&nbsp;", $related_field_title);
-			$select .= $this->build_concat_from_template(
-				$related_field_title,
-				$this->ESCAPE_CHAR,
-				$this->ESCAPE_CHAR,
-				$this->protect_identifiers($field_name_hash)
-			);
-			//$select .= "CONCAT('".str_replace(array('{','}'),array("',COALESCE(".$this->ESCAPE_CHAR , $this->ESCAPE_CHAR.", ''),'"),str_replace("'","\\'", $related_field_title))."') as ".$this->protect_identifiers($field_name_hash);
+			$select .= "CONCAT('".str_replace(array('{','}'),array("',COALESCE(",", ''),'"),str_replace("'","\\'",$related_field_title))."') as $field_name_hash";
 		}
 		else
 		{
-			$select .= $this->protect_identifiers($related_table.'.'.$related_field_title).' as '.$this->protect_identifiers($field_name_hash);
+			$select .= "$related_table.$related_field_title as $field_name_hash";
 		}
 
 		$this->db->select($select,false);
@@ -225,7 +298,7 @@ class Grocery_crud_model {
 			$this->db->limit($limit);
 
 		if($search_like !== null)
-			$this->db->having($this->protect_identifiers($field_name_hash)." LIKE '%".$this->db->escape_like_str($search_like)."%'");
+			$this->db->having("$field_name_hash LIKE '%".$this->db->escape_like_str($search_like)."%'");
 
 		$order_by !== null
 			? $this->db->order_by($order_by)
@@ -241,6 +314,19 @@ class Grocery_crud_model {
 		return $relation_array;
 	}
 
+	function get_ajax_relation_array($search, $field_name , $related_table , $related_field_title, $where_clause, $order_by)
+	{
+		return $this->get_relation_array($field_name , $related_table , $related_field_title, $where_clause, $order_by, 10 , $search);
+	}
+
+	function get_relation_total_rows($field_name , $related_table , $related_field_title, $where_clause)
+	{
+		if($where_clause !== null)
+			$this->db->where($where_clause);
+
+		return $this->db->count_all_results($related_table);
+	}
+
 	function get_relation_n_n_selection_array($primary_key_value, $field_info)
 	{
 		$select = "";
@@ -250,13 +336,7 @@ class Grocery_crud_model {
 		if($use_template)
 		{
 			$related_field_title = str_replace(" ", "&nbsp;", $related_field_title);
-			$select .= $this->build_concat_from_template(
-				$related_field_title,
-				$this->ESCAPE_CHAR,
-				$this->ESCAPE_CHAR,
-				$this->protect_identifiers($field_name_hash)
-			);
-			//$select .= "CONCAT('".str_replace(array('{','}'),array("',COALESCE(",", ''),'"),str_replace("'","\\'",$related_field_title))."') as $field_name_hash";
+			$select .= "CONCAT('".str_replace(array('{','}'),array("',COALESCE(",", ''),'"),str_replace("'","\\'",$related_field_title))."') as $field_name_hash";
 		}
 		else
 		{
@@ -304,13 +384,7 @@ class Grocery_crud_model {
 		if($use_template)
 		{
 			$related_field_title = str_replace(" ", "&nbsp;", $related_field_title);
-			$select .= $this->build_concat_from_template(
-				$related_field_title,
-				$this->ESCAPE_CHAR,
-				$this->ESCAPE_CHAR,
-				$this->protect_identifiers($field_name_hash)
-			);
-			//$select .= "CONCAT('".str_replace(array('{','}'),array("',COALESCE(",", ''),'"),str_replace("'","\\'",$related_field_title))."') as $field_name_hash";
+			$select .= "CONCAT('".str_replace(array('{','}'),array("',COALESCE(",", ''),'"),str_replace("'","\\'",$related_field_title))."') as $field_name_hash";
 		}
 		else
 		{
@@ -335,6 +409,49 @@ class Grocery_crud_model {
 		}
 
 		return $results_array;
+	}
+
+	function db_relation_n_n_update($field_info, $post_data ,$main_primary_key)
+	{
+		$this->db->where($field_info->primary_key_alias_to_this_table, $main_primary_key);
+		if(!empty($post_data))
+			$this->db->where_not_in($field_info->primary_key_alias_to_selection_table , $post_data);
+		$this->db->delete($field_info->relation_table);
+
+		$counter = 0;
+		if(!empty($post_data))
+		{
+			foreach($post_data as $primary_key_value)
+			{
+				$where_array = array(
+					$field_info->primary_key_alias_to_this_table => $main_primary_key,
+					$field_info->primary_key_alias_to_selection_table => $primary_key_value,
+				);
+
+				$this->db->where($where_array);
+				$count = $this->db->from($field_info->relation_table)->count_all_results();
+
+				if($count == 0)
+				{
+					if(!empty($field_info->priority_field_relation_table))
+						$where_array[$field_info->priority_field_relation_table] = $counter;
+
+					$this->db->insert($field_info->relation_table, $where_array);
+
+				}elseif($count >= 1 && !empty($field_info->priority_field_relation_table))
+				{
+					$this->db->update( $field_info->relation_table, array($field_info->priority_field_relation_table => $counter) , $where_array);
+				}
+
+				$counter++;
+			}
+		}
+	}
+
+	function db_relation_n_n_delete($field_info, $main_primary_key)
+	{
+		$this->db->where($field_info->primary_key_alias_to_this_table, $main_primary_key);
+		$this->db->delete($field_info->relation_table);
 	}
 
 	function get_field_types_basic_table()
@@ -373,30 +490,20 @@ class Grocery_crud_model {
 		return $results;
 	}
 
+	function db_update($post_array, $primary_key_value)
+	{
+		$primary_key_field = $this->get_primary_key();
+		return $this->db->update($this->table_name,$post_array, array( $primary_key_field => $primary_key_value));
+	}
+
 	function db_insert($post_array)
 	{
 		$insert = $this->db->insert($this->table_name,$post_array);
 		if($insert)
 		{
-			$primary_key = $this->get_primary_key();
-			// if user already define a value for the primary key, then just return it
-			// postgresql use LASTVAL() to retrieve insert_id which would cause an error if the sequence is not used.
-			if(array_key_exists($primary_key, $post_array)){
-				return $post_array[$primary_key];
-			}
 			return $this->db->insert_id();
 		}
 		return false;
-	}
-
-	function build_db_join_relation($related_table, $unique_name, $related_primary_key, $field_name){
-		$this->db->join($this->protect_identifiers($related_table).' as '.$this->protect_identifiers($unique_name) , $this->protect_identifiers($unique_name.'.'.$related_primary_key).' = '. $this->protect_identifiers($this->table_name.'.'.$field_name),'left');
-	}
-
-	function build_relation_n_n_subquery($field, $selection_table, $relation_table, $primary_key_alias_to_selection_table, $primary_key_selection_table, $primary_key_alias_to_this_table, $field_name){
-		return "(SELECT GROUP_CONCAT(DISTINCT ".$this->protect_identifiers($field).") FROM ".$this->protect_identifiers($selection_table)
-			." LEFT JOIN ".$this->protect_identifiers($relation_table)." ON ".$this->protect_identifiers($relation_table.".".$primary_key_alias_to_selection_table)." = ".$this->protect_identifiers($selection_table.".".$primary_key_selection_table)
-			." WHERE ".$this->protect_identifiers($relation_table.".".$primary_key_alias_to_this_table)." = ".$this->protect_identifiers($this->table_name.".".$this->get_primary_key($this->table_name))." GROUP BY ".$this->protect_identifiers($relation_table.".".$primary_key_alias_to_this_table).") AS ".$this->protect_identifiers($field_name);
 	}
 
 	function db_delete($primary_key_value)
@@ -406,11 +513,24 @@ class Grocery_crud_model {
 		if($primary_key_field === false)
 			return false;
 
+		$this->db->limit(1);
 		$this->db->delete($this->table_name,array( $primary_key_field => $primary_key_value));
 		if( $this->db->affected_rows() != 1)
 			return false;
 		else
 			return true;
+	}
+
+	function db_file_delete($field_name, $filename)
+	{
+		if( $this->db->update($this->table_name,array($field_name => ''),array($field_name => $filename)) )
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
 	}
 
 	function field_exists($field,$table_name = null)
@@ -419,23 +539,62 @@ class Grocery_crud_model {
 		{
 			$table_name = $this->table_name;
 		}
-
-		// sqlite doesn't support this $this->db->field_exists($field,$table_name)
-		$field_data_list = $this->db->field_data($table_name);
-		foreach($field_data_list as $field_data){
-			if($field_data->name == $field) return TRUE;
-		}
-		return FALSE;
+		return $this->db->field_exists($field,$table_name);
 	}
 
-	function get_edit_values($primary_key_value)
+	function get_primary_key($table_name = null)
 	{
-		$result = parent::get_edit_values($primary_key_value);
-		// some driver like postgresql doesn't return string
-		foreach($result as $key => $value) {
-			$result->$key = (string)$value;
+		if($table_name == null)
+		{
+			if(isset($this->primary_keys[$this->table_name]))
+			{
+				return $this->primary_keys[$this->table_name];
+			}
+
+			if(empty($this->primary_key))
+			{
+				$fields = $this->get_field_types_basic_table();
+
+				foreach($fields as $field)
+				{
+					if($field->primary_key == 1)
+					{
+						return $field->name;
+					}
+				}
+
+				return false;
+			}
+			else
+			{
+				return $this->primary_key;
+			}
 		}
-		return $result;
+		else
+		{
+			if(isset($this->primary_keys[$table_name]))
+			{
+				return $this->primary_keys[$table_name];
+			}
+
+			$fields = $this->get_field_types($table_name);
+
+			foreach($fields as $field)
+			{
+				if($field->primary_key == 1)
+				{
+					return $field->name;
+				}
+			}
+
+			return false;
+		}
+
+	}
+
+	function escape_str($value)
+	{
+		return $this->db->escape_str($value);
 	}
 
 }
