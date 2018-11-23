b0fd2d2070323902469217f7533645ab3fc81db5
Pedro de León <dleonmxn@hotmail.com>
2018-11-22 23:01:48 -0600

    Update orders-update.php

--- orders-update.php
+++ orders-update.php
@@ -22,7 +22,7 @@ if(isset($_SESSION['cart'])) {
 
         $query = pg_query($db_connection,
 			"INSERT INTO orders  (price, units, total, date, idProduct, idUsuario)
-					VALUES( " .$array['price'] .",".$quantity.",". $cost . ",2017-03-14,". $quantity . ", 1 ,1)");
+					VALUES( " .$array['price'] .",$quantity, $cost  ,'2017-03-14',1 ,1 )");
 
         if($query){
           $newqty = $array['qty'] - $quantity;
