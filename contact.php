4e9b8fafa483443f610f01125640fe13154b866a
Pedro de León <dleonmxn@hotmail.com>
2018-11-24 11:53:57 -0600

    Update contact.php

--- contact.php
+++ contact.php
@@ -268,6 +268,8 @@ if(session_id() == '' || !isset($_SESSION)){session_start();}
     <script src="js/vendor/jquery.js"></script>
     <script src="js/foundation.min.js"></script>
     <script src="js/bienvenida.js"></script>
+    <script src="js/contact.js.js"></script>
+
     <script>
       $(document).foundation();
     </script>
