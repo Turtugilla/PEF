849cd1e4e4a86f3f99abf3a608535ae8fc1cab4c
Pedro de León <dleonmxn@hotmail.com>
2018-11-24 00:05:34 -0600

    Update surtir-huevos.php

--- surtir-huevos.php
+++ surtir-huevos.php
@@ -1,18 +1,16 @@
 <?php
+
+include('config.php');
 header('Access-Control-Allow-Origin: *');
-$valorPasteles=json_decode($_GET["huevos"],true);
+$valorHuevos=json_decode($_GET["huevos"],true);
 
-function pg_connection_string_from_database_url() {
-  extract(parse_url($_ENV["DATABASE_URL"]));
-  return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
-}
 
-$pg_conn = pg_connect($conn.pg_connection_string_from_database_url());
+
+
 
 
 foreach( $valorHuevos as $huevo ) {
-	
-		$result = pg_query($pg_conn, "SELECT  qty  FROM products WHERE product_name = ".$huevo["nombre_producto"]);
+		$result = pg_query($db_connection, "SELECT  qty  FROM products WHERE product_name = ".$huevo["nombre_producto"]);
 		$c = pg_fetch_row($result);
 		$array = array();
 		if($c[0] >= $huevo["cantidad"]){
