4504ef83e76e0e23cd7187775f1abd72ff3ba1c9
Pedro de León <dleonmxn@hotmail.com>
2018-11-24 00:09:15 -0600

    Update surtir-huevos.php

--- surtir-huevos.php
+++ surtir-huevos.php
@@ -8,14 +8,14 @@ $valorHuevos=json_decode($_GET["huevos"],true);
 
 
 
-
+$array = array();
 foreach( $valorHuevos as $huevo ) {
 		$result = pg_query($db_connection, "SELECT  qty  FROM products WHERE product_name = ".$huevo["nombre_producto"]);
 		$c = pg_fetch_row($result);
-		$array = array();
+
 		if($c[0] >= $huevo["cantidad"]){
               array_push($array, 'success');
-              $update = pg_query($db_connection, 
+              $update = pg_query($db_connection,
 				  "UPDATE products SET qty=".($c[0]- $huevo["cantidad"])." WHERE product_name=".$huevo["nombre_producto"]);
 		}else{
 			  array_push($array, 'error');
