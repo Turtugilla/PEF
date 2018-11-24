0e959b2d290794ced8d9f20acaeb4131dc333d45
Pedro de León <dleonmxn@hotmail.com>
2018-11-23 19:04:47 -0600

    actuliazando enviar email

--- send-mail.php
+++ send-mail.php
@@ -18,18 +18,20 @@ $mensaje = $_POST["mensaje"];
 
 
 
-$mail= new PHPMailer();
 
 
 
+/*
+ * $mail= new PHPMailer();
+ *
 $mail->isSMTP();
 $mail->Port = 587;
 $mail->Host = 'smtp.gmail.com';
 $mail->SMTPAuth = true;
 $mail->SMTPSecure = 'tls';
 
-/*En estas dos siguientes lineas va el código de las credenciales de email que
- va hostear el enviar mensajes por correo electrónico */
+En estas dos siguientes lineas va el código de las credenciales de email que
+ va hostear el enviar mensajes por correo electrónico
 $mail->Username = 'dleonmxn@gmail.com';
 $mail->Password = 'tigres100%';
 
@@ -41,6 +43,17 @@ $mail->addAddress("angel.ygtp@gmail.com","Angel Acosta");
 $mail->Subject = $subject;
 $mail->Body =  $mensaje;
 $mail->send();
+*/
+
+$sendgrid = new SendGrid("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");
+$email    = new SendGrid\Email();
+
+$email->addTo("app115927986@heroku.com")
+	->setFrom($email)
+	->setSubject($subject)
+	->setHtml($mensaje);
+
+$sendgrid->send($email);
 
 
 
